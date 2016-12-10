78779d93705d3cfa6321790359228bc9ccf63103
Naif Said <E7sas_nano@hotmail.com>
2016-12-09 20:27:43 -0800

    add

---
+++ Lab1/js/script.js
@@ -0,0 +1,15 @@
+//Your JavaScript code here
+
+
+		function myFunction(){
+
+		var theAnswer = document.theForm.theAnswer.value;
+
+		if(theAnswer == "2"){
+			window.alert("bravo!!!");
+		}
+		else {
+			window.alert("boooooo!!!");
+		}
+		}
+
